494334514cdb583348b191a31b34f2183566512a
Breno Sales <salesonerb18.22@gmail.com>
2025-04-12 21:04:57 -0300

    controller auth verifica o login do app

---
+++ controllers/auth.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"nbapp/config"
+	"nbapp/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type LoginInput struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func Login(c *gin.Context) {
+	var input LoginInput
+	if err := c.ShouldBindBodyWithJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Dados inválidos."})
+		return
+	}
+
+	var user models.User
+	if err := config.DB.Where("username = ? AND password = ?", input.Username, input.Password).First(&user).Error; err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Usuário ou senha inválidos."})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Login realiado com sucesso!"})
+}
